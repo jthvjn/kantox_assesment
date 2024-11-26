5ba2ed87c6b53a626878a20a29370984de71c3b6
ajith.vijayan <jthvjn@gmail.com>
2024-11-26 11:01:24 +0530

    updated cf1 tests

--- test/discounting/cf1_test.exs
+++ test/discounting/cf1_test.exs
@@ -1,17 +1,25 @@
 defmodule Discounting.Cf1Test do
   use ExUnit.Case
 
+  @price 11.23
+
   alias Discounting.DiscountingRules.Cf1
 
   test "If the quantity is 3 or more coffees, the price of all coffees should drop to two thirds of the original price." do
-    expected_price = 11.23 * 5 * 2 / 3
+    quantity = 5
+    expected_price = @price * quantity * 2 / 3
 
-    assert {:ok, expected_price} == Cf1.apply(11.23, 5)
+    assert {:ok, expected_price} == Cf1.apply(@price, quantity)
   end
 
   test "If the quantity is less than 3 coffees, no discount." do
-    expected_price = 11.23 * 5 * 2 / 3
+    quantity = 2
+    expected_price = @price * quantity
 
-    assert {:ok, expected_price} == Cf1.apply(11.23, 5)
+    assert {:ok, expected_price} == Cf1.apply(@price, quantity)
+  end
+
+  test "returns invalid error on non integer quantity" do
+    assert {:error, :invalid} == Cf1.apply(11.23, 5.5)
   end
 end
