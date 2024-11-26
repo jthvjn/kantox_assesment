1b5cd65ee379f4c46e7e2a0815899bc7a3d8a2f0
ajith.vijayan <jthvjn@gmail.com>
2024-11-26 10:03:48 +0530

    initial setup

---
+++ lib/checkout_counter.ex
@@ -0,0 +1,18 @@
+defmodule CheckoutCounter do
+  @moduledoc """
+  Documentation for `CheckoutCounter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CheckoutCounter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/checkout_counter_test.exs
@@ -0,0 +1,8 @@
+defmodule CheckoutCounterTest do
+  use ExUnit.Case
+  doctest CheckoutCounter
+
+  test "greets the world" do
+    assert CheckoutCounter.hello() == :world
+  end
+end
