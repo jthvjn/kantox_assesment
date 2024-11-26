f7748648145baf0c0fe0772d5608feba3e36fab0
ajith.vijayan <jthvjn@gmail.com>
2024-11-26 17:50:26 +0530

    Added checkout counter

--- lib/checkout_counter.ex
+++ lib/checkout_counter.ex
@@ -1,18 +1,37 @@
 defmodule CheckoutCounter do
   @moduledoc """
-  Documentation for `CheckoutCounter`.
+  Utils for checkout counter
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CheckoutCounter.hello()
-      :world
-
+  Gnerates the the net total for the items in the list
+  after applying discount rules.
   """
-  def hello do
-    :world
+  @spec generate_bill(any()) :: float()
+  def generate_bill(item_codes) do
+    if Inventory.items_available?(item_codes) do
+      {:ok,
+       calculate_net_total(item_codes)
+       |> Decimal.to_float()}
+    else
+      {:error, :invalid_items}
+    end
+  end
+
+  defp calculate_net_total(item_codes) do
+    item_codes
+    |> Enum.frequencies()
+    |> Enum.reduce(Decimal.new(0), fn {item_code, quantity}, total ->
+      {:ok, price} = Inventory.get_price(item_code)
+      {:ok, net_price} = Discounting.Manager.apply_discount(item_code, price, quantity)
+
+      total =
+        net_price
+        |> Decimal.from_float()
+        |> Decimal.add(total)
+        |> Decimal.round(2, :half_up)
+
+      total
+    end)
   end
 end
--- test/checkout_counter_test.exs
+++ test/checkout_counter_test.exs
@@ -1,8 +1,14 @@
 defmodule CheckoutCounterTest do
   use ExUnit.Case
-  doctest CheckoutCounter
 
-  test "greets the world" do
-    assert CheckoutCounter.hello() == :world
+  test "generate_bill/1, returns net total after discounting" do
+    assert {:ok, 22.45} == CheckoutCounter.generate_bill(["GR1", "SR1", "GR1", "GR1", "CF1"])
+    assert {:ok, 3.11} == CheckoutCounter.generate_bill(["GR1", "GR1"])
+    assert {:ok, 16.61} == CheckoutCounter.generate_bill(["SR1", "SR1", "GR1", "SR1"])
+    assert {:ok, 30.57} == CheckoutCounter.generate_bill(["GR1", "CF1", "SR1", "CF1", "CF1"])
+  end
+
+  test "generate_bill/1, on invalid item codes" do
+    assert {:error, :invalid_items} == CheckoutCounter.generate_bill(["SS1"])
   end
 end
